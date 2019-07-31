4f8deef49fec7de032e52bb9b873e769f5a85329
Rodrigo Sartori <rodrigosartoriazevedo@gmail.com>
2019-07-31 11:05:41 -0300

    Inserido os Produtos das categorias e associação muitos para muitos

--- src/main/java/com/rodrigo/cursomc/domain/Categoria.java
+++ src/main/java/com/rodrigo/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.rodrigo.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -13,18 +16,27 @@ public class Categoria implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy ="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
 
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
 
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 	public int getId() {
 		return id;
 	}
@@ -62,6 +74,8 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
