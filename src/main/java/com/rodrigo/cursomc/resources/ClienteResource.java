28aa943f3227632ec5d268622714c5fa7870db00
Rodrigo Sartori <rodrigosartoriazevedo@gmail.com>
2019-07-31 22:27:41 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/rodrigo/cursomc/resources/ClienteResource.java
@@ -0,0 +1,40 @@
+package com.rodrigo.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigo.cursomc.domain.Cliente;
+import com.rodrigo.cursomc.services.ClienteService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@RestController 
+@RequestMapping(value = "/clientes")
+
+
+
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
+//	@GetMapping("/{id}")
+	public ResponseEntity<?> find (@PathVariable Integer id) throws ObjectNotFoundException{	
+			
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+/**	@PostMapping()
+	
+	@PutMapping
+	
+	@DeleteMapping
+	
+	**/
+}
